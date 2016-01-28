f0743a0d09379fcb73cae70b9bfe199484b3579e
Tomas <tomasrogberg@gmail.com>
2016-01-28 13:58:21 +0100

    new files

---
+++ Garage2/Models/VehicleType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
