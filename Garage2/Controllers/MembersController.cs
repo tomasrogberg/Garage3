60403c63e178136b21edcc1ad1c9b680f5ad82ef
urbinao <oscar.urbina@telia.com>
2016-01-29 10:19:23 +0100

    members.tolist

---
+++ Garage2/Controllers/MembersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Garage2.Models;
+
+namespace Garage2.Controllers
+{
+    public class MembersController : Controller
+    {
+        private Garage2Context db = new Garage2Context();
+
+        // GET: Members
+        public ActionResult Index()
+        {
+            return View(db.Members.ToList());
+        }
+
+        // GET: Members/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Members/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Members/Createusing System;
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Members/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Members/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Members/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Members/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
