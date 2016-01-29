f1961afad568a8b6117ff98860aa4bdd0ecc2000
Tomas <tomasrogberg@gmail.com>
2016-01-29 13:08:29 +0100

    index

--- Garage2/Controllers/Vehicles2Controller.cs
+++ Garage2/Controllers/Vehicles2Controller.cs
@@ -15,11 +15,82 @@ namespace Garage2.Controllers
         private Garage2Context db = new Garage2Context();
 
         // GET: Vehicles2
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    var vehicles = db.Vehicles.Include(v => v.Member).Include(v => v.Type);
+        //    return View(vehicles.ToList());
+        //}
+
+
+        public ActionResult Index(string searchString, string sortOrder)
         {
-            var vehicles = db.Vehicles.Include(v => v.Member).Include(v => v.Type);
-            return View(vehicles.ToList());
+
+            //ViewBag.TypeSortParm = sortOrder == "type_desc" ? "type_asc" : "type_desc";
+            ViewBag.RegNrSortParm = sortOrder == "regnr_desc" ? "regnr_asc" : "regnr_desc";
+            ViewBag.BrandSortParm = sortOrder == "brand_desc" ? "brand_asc" : "brand_desc";
+            ViewBag.CheckinTimeSortParm = sortOrder == "checkintime_desc" ? "checkintime_asc" : "checkintime_desc";
+            ViewBag.ParkingTimeSortParm = sortOrder == "parkingtime_desc" ? "parkingtime_asc" : "parkingtime_desc";
+            ViewBag.SlotNoSortParm = sortOrder == "slotno_desc" ? "slotno_asc" : "slotno_desc";
+            ViewBag.SearchString = searchString;
+
+            //var vehicle = db.Vehicles.Include(v => v.Member).Include(v => v.Type);
+            var vehicle = from v in db.Vehicles.Include(v => v.Member).Include(v => v.Type)
+                          select v;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                vehicle = vehicle.Where(s => s.RegNr.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "regnr_desc":
+                    vehicle = vehicle.OrderByDescending(v => v.RegNr);
+                    break;
+                case "regnr_asc":
+                    vehicle = vehicle.OrderBy(v => v.RegNr);
+                    break;
+                //case "type_desc":
+                //    vehicle = vehicle.OrderByDescending(v => v.Type);
+                //    break;
+                //case "type_asc":
+                //    vehicle = vehicle.OrderBy(v => v.Type);
+                //    break;
+                case "brand_desc":
+                    vehicle = vehicle.OrderByDescending(v => v.Brand);
+                    break;
+                case "brand_asc":
+                    vehicle = vehicle.OrderBy(v => v.Brand);
+                    break;
+                case "checkintime_desc":
+                    vehicle = vehicle.OrderByDescending(v => v.CheckInTime);
+                    break;
+                case "checkintime_asc":
+                    vehicle = vehicle.OrderBy(v => v.CheckInTime);
+                    break;
+                case "parkingtime_desc":
+                    vehicle = vehicle.OrderByDescending(v => v.CheckInTime);
+                    break;
+                case "parkingtime_asc":
+                    vehicle = vehicle.OrderBy(v => v.CheckInTime);
+                    break;
+                case "slotno_desc":
+                    vehicle = vehicle.OrderByDescending(v => v.ParkNr);
+                    break;
+                case "slotno_asc":
+                    vehicle = vehicle.OrderBy(v => v.ParkNr);
+                    break;
+
+                default:
+                    vehicle = vehicle.OrderBy(v => v.RegNr);
+                    break;
+            }
+            List<Vehicle> vehicleList = vehicle.ToList();
+            return View(vehicleList);
+
+
         }
+
 
         // GET: Vehicles2/Details/5
         public ActionResult Details(int? id)
@@ -64,6 +135,7 @@ namespace Garage2.Controllers
                     vehicle.MemberId = mem.Id;
                     if (ModelState.IsValid)
                     {
+                        vehicle.CheckInTime = DateTime.Now;
                         db.Vehicles.Add(vehicle);
                         db.SaveChanges();
                         return RedirectToAction("Index");
