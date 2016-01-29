6eafd25ed25f685d89e45fc721660df9234480b6
Tomas <tomasrogberg@gmail.com>
2016-01-29 16:05:49 +0100

    kvittofix och sortering

--- Garage2/Controllers/Vehicles2Controller.cs
+++ Garage2/Controllers/Vehicles2Controller.cs
@@ -24,8 +24,8 @@ namespace Garage2.Controllers
 
         public ActionResult Index(string searchString, string sortOrder, string searchType)
         {
-
-            //ViewBag.TypeSortParm = sortOrder == "type_desc" ? "type_asc" : "type_desc";
+            ViewBag.NameSortParm = sortOrder == "name_desc" ? "name_asc" : "name_desc";
+            ViewBag.TypeSortParm = sortOrder == "type_desc" ? "type_asc" : "type_desc";
             ViewBag.RegNrSortParm = sortOrder == "regnr_desc" ? "regnr_asc" : "regnr_desc";
             ViewBag.BrandSortParm = sortOrder == "brand_desc" ? "brand_asc" : "brand_desc";
             ViewBag.CheckinTimeSortParm = sortOrder == "checkintime_desc" ? "checkintime_asc" : "checkintime_desc";
@@ -45,23 +45,29 @@ namespace Garage2.Controllers
 
             if (!String.IsNullOrEmpty(searchType))
             {
-                vehicle = vehicle.Where(s => s.Type.ToString().Contains(searchType));
+                vehicle = vehicle.Where(s => s.Type.Type.Contains(searchType));
             }
 
             switch (sortOrder)
             {
+                case "name_desc":
+                    vehicle = vehicle.OrderByDescending(v => v.Member.Name);
+                    break;
+                case "name_asc":
+                    vehicle = vehicle.OrderBy(v => v.Member.Name);
+                    break;
                 case "regnr_desc":
                     vehicle = vehicle.OrderByDescending(v => v.RegNr);
                     break;
                 case "regnr_asc":
                     vehicle = vehicle.OrderBy(v => v.RegNr);
                     break;
-                //case "type_desc":
-                //    vehicle = vehicle.OrderByDescending(v => v.Type);
-                //    break;
-                //case "type_asc":
-                //    vehicle = vehicle.OrderBy(v => v.Type);
-                //    break;
+                case "type_desc":
+                    vehicle = vehicle.OrderByDescending(v => v.Type.Type);
+                    break;
+                case "type_asc":
+                    vehicle = vehicle.OrderBy(v => v.Type.Type);
+                    break;
                 case "brand_desc":
                     vehicle = vehicle.OrderByDescending(v => v.Brand);
                     break;
@@ -105,6 +111,7 @@ namespace Garage2.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            //Vehicle vehicle = db.Vehicles.Include(v => v.Member).Include(v => v.Type);
             Vehicle vehicle = db.Vehicles.Find(id);
             if (vehicle == null)
             {
@@ -198,6 +205,7 @@ namespace Garage2.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            
             Vehicle vehicle = db.Vehicles.Find(id);
             if (vehicle == null)
             {
@@ -209,13 +217,57 @@ namespace Garage2.Controllers
         // POST: Vehicles2/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Vehicle vehicle = db.Vehicles.Find(id);
+        //    db.Vehicles.Remove(vehicle);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
         public ActionResult DeleteConfirmed(int id)
         {
+
+            //var vehicles = db.Vehicles.Include(v => v.Member).Include(v => v.Type);
+           // Vehicle vehicle = vehicle.fin
+            //var vehicle = db.Vehicles.Include(v => v.Member).Include(v => v.Type).Find(id);
+
             Vehicle vehicle = db.Vehicles.Find(id);
+
+            // DeleteFromGarage(vehicle);
+
             db.Vehicles.Remove(vehicle);
             db.SaveChanges();
-            return RedirectToAction("Index");
+
+            return RedirectToAction("Receipt", vehicle);
+
         }
+
+        public ActionResult Receipt(Vehicle vehicle)
+        {
+
+            DateTime CheckOutTime = DateTime.Now;
+            TimeSpan? ParkingTime = new TimeSpan();
+
+            ParkingTime = CheckOutTime - vehicle.CheckInTime;
+
+            double totH = ParkingTime.Value.TotalHours;
+            int totHours = Convert.ToInt32(Math.Truncate(totH));
+
+            var result = string.Format("{0:D2}:{1:D2}", totHours, ParkingTime.Value.Minutes);
+
+            double pay = ParkingTime.Value.TotalHours * 60;
+            string Payment = String.Format("{0:0}", pay);
+
+            ViewBag.CheckOutTime = CheckOutTime;
+            ViewBag.Payment = Payment;
+            ViewBag.ParkingTime = result;
+            ViewBag.MemberName = db.Members.Where(m => m.Id == vehicle.MemberId).FirstOrDefault().Name;
+            ViewBag.VehType = db.VehicleTypes.Where(m => m.Id == vehicle.VTypeId).FirstOrDefault().Type;
+            return View(vehicle);
+
+        }
+
 
         protected override void Dispose(bool disposing)
         {
